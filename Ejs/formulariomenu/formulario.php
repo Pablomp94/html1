f6a31a26d29091ea1d2e18e567dc26d245e3cccc
Pablomp94 <1744623@alu.murciaeduca.es>
2023-10-06 09:23:17 +0200

    html

--- Ejs/formulariomenu/formulario.php
+++ Ejs/formulariomenu/formulario.php
@@ -5,6 +5,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>BIENVENIDO:</h1>
+    <h2><?php echo $_POST["nombre"];?></h2>
+
+    <h1>Su correo es:</h1>
+    <h2><?php echo $_POST["email"];?> </h2>
+
+    <h1>Su contraseña es:</h1>
+    <h2><?php echo $_POST["contraseña"];?></h2>
+
+    <h1>Su género es:</h1>
+    <h2><?php echo $_POST["genero"];?></h2>
+
+    <h1>Te interesa:</h1>
+    <h2><?php echo $_POST["intereses"];?></h2>
+
+    <h1>Vives en:</h1>
+    <h2><?php echo $_POST["Pais"];?></h2>
+
+    <h1>Nacieste el:</h1>
+    <h2><?php echo $_POST["nacimiento"];?></h2>
 </body>
 </html>
